3cadf57dbf983491f69d7563c73bd104be6246d6
Wallace Santana <wallacesantanak0@gmail.com>
2025-03-26 15:07:58 -0300

    (feat): uniqueBST

---
+++ uniqueBST/main.c
@@ -0,0 +1,18 @@
+int numTrees(int n) {
+    if (n <= 1) return 1;
+
+    int* dp = (int*)calloc(n + 1, sizeof(int));
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            dp[i] += dp[j - 1] * dp[i - j];
+        }
+    }
+
+    int result = dp[n];
+    free(dp);
+    return result;
+}
+

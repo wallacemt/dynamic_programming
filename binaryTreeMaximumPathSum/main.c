a06338e6d96149fca9b27a77ee39c21192df3801
Wallace Santana <wallacesantanak0@gmail.com>
2025-03-29 11:48:08 -0300

    (feat): binaryTreeMaximum

---
+++ binaryTreeMaximumPathSum/main.c
@@ -0,0 +1,24 @@
+#include <limits.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+int maxPathSumUtil(struct TreeNode* root, int* result) {
+    if (root == NULL) return 0;
+
+    int left = fmax(0, maxPathSumUtil(root->left, result));
+    int right = fmax(0, maxPathSumUtil(root->right, result));
+
+    *result = fmax(*result, left + right + root->val);
+
+    return root->val + fmax(left, right);
+}
+
+int maxPathSum(struct TreeNode* root) {
+    int result = INT_MIN;
+    maxPathSumUtil(root, &result);
+    return result;
+}
